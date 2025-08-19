b339d494e6dfa1573d6b23ef928dcdf62bd0f9f6
keaganllee <zeroediv@gmail.com>
2025-08-20 01:23:00 +0800

    api connects

---
+++ Inference/inference_api.py
@@ -0,0 +1,23 @@
+# inference_api.py
+from fastapi import FastAPI
+from pydantic import BaseModel
+from inference import predict_from_clean
+
+app = FastAPI()
+
+class InferReq(BaseModel):
+    test_clean_filename: str = "test_clean.csv"
+    model_filename: str = "model.pkl"
+    output_filename: str = "test_with_preds.csv"
+    target_col: str = "Survived"
+
+@app.post("/predict")
+def predict(req: InferReq):
+    out = predict_from_clean(
+        req.test_clean_filename, req.model_filename, req.output_filename, req.target_col
+    )
+    return {"status": "ok", "predictions_file": out}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8002)
